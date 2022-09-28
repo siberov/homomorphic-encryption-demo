5ef309afa533b01633c06bfb29eec635b0e43c00
Arvid Siberov <arvidsiberov@outlook.com>
2022-09-28 11:10:31 +0200

    Add working prototype
    No error handling and a lot of hardcoded stuff, but we at
    least shows the SHA of the data on the server to display
    that the numbers are different each time. (Try 0 + 0)

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -2,34 +2,41 @@ use concrete::{ConfigBuilder, generate_keys, set_server_key, FheUint8};
 use concrete::prelude::*;
 use std::net::TcpStream;
 use bincode::{self, serialize_into};
-// use serde_json::{Serializer, Deserializer};
-// use reqwest::blocking::Client;
+use std::env;
 
 mod common;
 
 fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage: client <a> <b>, to calculate <a> + <b>.\nBoth <a> and <b> must be in the range 0 .. 255");
+        return
+    }
+    let clear_a: u8 = str::parse(&args[1]).unwrap();
+    let clear_b: u8 = str::parse(&args[2]).unwrap();
+
+    let addr = "127.0.0.1:50000";
+    let stream = TcpStream::connect(addr).unwrap();
+    println!("Encrypting input and generating keys. This may take a while ...");
+
     let config = ConfigBuilder::all_disabled()
         .enable_default_uint8()
         .build();
-    println!("Config built");
 
     let (client_key, server_key) = generate_keys(config);
-    println!("Keys generated");
 
     let server_key_clone = server_key.clone();
     set_server_key(server_key);
 
-    let clear_a = 27u8;
-    let clear_b = 128u8;
-
-    println!("Encrypting ...");
     let a = FheUint8::encrypt(clear_a, &client_key);
-    println!("Encrypted a!");
     let b = FheUint8::encrypt(clear_b, &client_key);
-    println!("Encrypted b!");
 
-    let addr = "127.0.0.1:50000";
-    let stream = TcpStream::connect(addr).unwrap();
+    println!("Finished encryption.");
+
+    println!("Serialized sizes\na: {} bytes\nb: {} bytes\nkey: {} bytes",
+        bincode::serialized_size(&a).unwrap(),
+        bincode::serialized_size(&b).unwrap(),
+        bincode::serialized_size(&server_key_clone).unwrap());
 
     let payload = common::Payload {
         a,
@@ -40,8 +47,7 @@ fn main() {
     serialize_into(&stream, &payload);
 
     let result : FheUint8 = bincode::deserialize_from(&stream).unwrap();
-
     let decrypted_result: u8 = result.decrypt(&client_key);
 
-    println!("Answer: {}", decrypted_result);
+    println!("Decrypted answer: {}", decrypted_result);
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,11 +1,13 @@
 use concrete::{set_server_key, FheUint8};
-use std::net::TcpListener;
+use std::{net::TcpListener, slice, mem};
+use sha2::{Sha256, Digest};
 
 mod common;
  
 fn main() -> Result<(), bincode::Error> {
+    println!("Waiting for connection ...");
     let (listener, _) = TcpListener::bind("127.0.0.1:50000").unwrap().accept().unwrap();
-    println!("Got connection!");
+    println!("Got connection.");
     let payload : common::Payload = bincode::deserialize_from(&listener).unwrap();
 
     set_server_key(payload.key.to_owned());
@@ -13,7 +15,23 @@ fn main() -> Result<(), bincode::Error> {
     let a = payload.a.clone();
     let b = payload.b.clone();
 
+    let a_bytes = unsafe { slice::from_raw_parts(&a as *const _ as *const u8, mem::size_of::<FheUint8>()) }.to_vec();
+    let b_bytes = unsafe { slice::from_raw_parts(&b as *const _ as *const u8, mem::size_of::<FheUint8>()) }.to_vec();
+
     let res : FheUint8 = a + b;
+
+    let res_bytes = unsafe { slice::from_raw_parts(&res as *const _ as *const u8, mem::size_of::<FheUint8>()) }.to_vec();
+
+    let mut hasher = Sha256::new();
+    hasher.update(a_bytes);
+    let a_hash = hasher.finalize();
+    hasher = Sha256::new();
+    hasher.update(b_bytes);
+    let b_hash = hasher.finalize();
+    hasher = Sha256::new();
+    hasher.update(res_bytes);
+    let res_hash = hasher.finalize();
+    println!("Hash of a:\t{:02X?}\nHash of b:\t{:02X?}\nHash of a + b:\t{:02X?}", a_hash, b_hash, res_hash);
 
     bincode::serialize_into(listener, &res)
 }
